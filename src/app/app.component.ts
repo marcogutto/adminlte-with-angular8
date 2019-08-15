128bf5d2da05afc08eaadb92a776c48c99918f79
marcogutto <marcogutto@gmail.com>
2019-08-15 10:10:27 -0300

    Configuring adminLTE packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adminlte-with-angular8';
+  bodyClasses = 'skin-blue sidebar-mini';
+  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+  ngOnInit() {
+   this.body.classList.add('skin-blue');
+   this.body.classList.add('sidebar-mini');
+  }
 }
